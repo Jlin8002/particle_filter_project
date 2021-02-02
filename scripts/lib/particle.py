13aa8a36d02e30fa50750fbf529431698ebf1449
Adam Weider <adamweider98@gmail.com>
2021-02-02 04:08:46 -0600

    use turtlebot pose for particles

--- scripts/lib/particle.py
+++ scripts/lib/particle.py
@@ -5,16 +5,16 @@ from typing import List
 import math
 import numpy.random as random
 
-from geometry_msgs.msg import Point, Pose
 from nav_msgs.msg import OccupancyGrid
-from tf.transformations import quaternion_from_euler
 
+from lib.turtle_bot import TurtlePose
 from lib.util import draw_uniform_sample
+from lib.vector2 import Vector2
 
 
 @dataclass
 class Particle:
-    pose: Pose
+    pose: TurtlePose
     weight: float
 
 
@@ -41,22 +41,15 @@ def from_occupancy_grid(grid: OccupancyGrid, num_particles: int) -> List[Particl
         col = index % grid.info.width
         row = index // grid.info.width
 
-        position = Point(
+        position = Vector2(
             x=col * grid.info.resolution,
             y=row * grid.info.resolution,
-            z=0.0,
         )
 
         yaw = rng.uniform(low=0.0, high=2.0 * math.pi)
 
-        orientation = quaternion_from_euler(
-            ai=0.0,
-            aj=0.0,
-            ak=yaw,
-        )
-
         return Particle(
-            pose=Pose(position, orientation),
+            pose=TurtlePose(position, yaw),
             weight=1.0 / _num_particles,
         )
 

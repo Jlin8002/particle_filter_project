11cca6b098f9a2c897e87b896ec719020087084c
Adam Weider <adamweider98@gmail.com>
2021-02-03 23:02:06 -0600

    fixes

--- scripts/particle_filter.py
+++ scripts/particle_filter.py
@@ -4,6 +4,7 @@ from functools import reduce
 from typing import List, Tuple
 
 import numpy as np
+import rospy
 
 from nav_msgs.msg import OccupancyGrid
 from sensor_msgs.msg import LaserScan
@@ -19,23 +20,23 @@ from lib.vector2 import Vector2
 
 
 class ParticleFilter(ParticleFilterBase):
-    @staticmethod
     def initialize_particle_cloud(
+        self,
         num_particles: int,
         occupancy_grid: OccupancyGrid,
     ) -> List[Particle]:
-        return particle.from_occupancy_grid(occupancy_grid, num_particles)
+        ps = particle.from_occupancy_grid(occupancy_grid, num_particles)
+        print(len(ps))
+        return ps
 
-    @staticmethod
-    def normalize_particles(particle_cloud: List[Particle]) -> List[Particle]:
+    def normalize_particles(self, particle_cloud: List[Particle]) -> List[Particle]:
         weight_sum = sum([p.weight for p in particle_cloud])
 
         return [
             Particle(pose=p.pose, weight=p.weight / weight_sum) for p in particle_cloud
         ]
 
-    @staticmethod
-    def resample_particles(particle_cloud: List[Particle]) -> List[Particle]:
+    def resample_particles(self, particle_cloud: List[Particle]) -> List[Particle]:
         weights = [p.weight for p in particle_cloud]
 
         return draw_weighted_sample(
@@ -44,41 +45,49 @@ class ParticleFilter(ParticleFilterBase):
             n=len(particle_cloud),
         )
 
-    @staticmethod
-    def update_estimated_robot_pose(particle_cloud: List[Particle]) -> TurtlePose:
+    def update_estimated_robot_pose(self, particle_cloud: List[Particle]) -> TurtlePose:
         def accumulate_components(
-            acc: Tuple[List[Vector2], List[float], List[float]],
+            acc: Tuple[List[Tuple[float, float]], List[float], List[float]],
             p: Particle,
         ):
             (ps, ys, ws) = acc
-            return ([p.pose.position, *ps], [p.pose.yaw, *ys], [p.weight, *ws])
+            pos_tup = (p.pose.position.x, p.pose.position.y)
+
+            return ([pos_tup, *ps], [p.pose.yaw, *ys], [p.weight, *ws])
 
         (positions, yaws, weights) = reduce(
             accumulate_components,
             particle_cloud,
-            initial=([], [], []),
+            ([], [], []),
         )
 
-        avg_position = np.average(positions, weights=weights)
+        avg_position = Vector2(
+            *np.average(positions, axis=0, weights=weights)
+        )  # TODO: which axis ?
         avg_yaw = np.average(yaws, weights=weights)
 
         return TurtlePose(position=avg_position, yaw=avg_yaw)
 
-    @staticmethod
     def update_particle_weights_with_measurement_model(
+        self,
         particle_cloud: List[Particle],
         laser_scan: LaserScan,
     ) -> List[Particle]:
         # TODO
-        pass
+        return particle_cloud
 
-    @staticmethod
     def update_particles_with_motion_model(
+        self,
         particle_cloud: List[Particle],
         disp_linear: Vector2,
         disp_angular: float,
     ) -> List[Particle]:
-        # TODO: Out of bounds check (probably create a 2d OccupancyMatrix datatype for convenience)
+        # TODO: Out of bounds check
         return [
             particle.translate(p, disp_linear, disp_angular) for p in particle_cloud
         ]
+
+
+if __name__ == "__main__":
+    ParticleFilter()
+    rospy.spin()

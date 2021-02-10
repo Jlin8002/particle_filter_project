e8c440975a6f2eb4e829d5bd7c71df286980813d
Adam Weider <adamweider98@gmail.com>
2021-02-10 04:40:26 -0600

    comments

--- scripts/lib/particle.py
+++ scripts/lib/particle.py
@@ -5,19 +5,14 @@ Particles for the particle cloud.
 # pyright: reportMissingTypeStubs=false
 
 from dataclasses import dataclass, replace
-from typing import List
 
-import math
 import numpy.random as random
-from nav_msgs.msg import OccupancyGrid
 from rospy_util.vector2 import Vector2
 import rospy_util.vector2 as v2
 
-import lib.cell as cell
 from lib.likelihood_field import LikelihoodField
 import lib.likelihood_field as lf
 from lib.turtle_pose import TurtlePose
-from lib.util import draw_uniform_sample, yaw_from_quaternion
 
 
 @dataclass
@@ -78,55 +73,3 @@ def sanitize(field: LikelihoodField, p: Particle) -> Particle:
     field.
     """
     return p if lf.at_free_pos(field, p.pose.position) else replace(p, weight=0.0)
-
-
-def from_occupancy_grid(grid: OccupancyGrid, num_particles: int) -> List[Particle]:
-    """
-    Create a particle cloud of size `num_particles` from the given occupancy grid.
-    """
-    cells_free = [ix for (ix, c) in enumerate(grid.data) if c == cell.FREE]
-
-    # Use no more than the number of free cells as the number of particles.
-    _num_particles = min(len(cells_free), num_particles)
-
-    # Uniformly sample free cells.
-    cells_selected = draw_uniform_sample(
-        choices=cells_free,
-        n=_num_particles,
-    )
-
-    rng = random.default_rng()
-
-    origin_pos = v2.from_point(grid.info.origin.position)
-    origin_yaw = yaw_from_quaternion(grid.info.origin.orientation)
-
-    def from_cell(index: int) -> Particle:
-        # Compute cell column and row using cell index and grid dimensions.
-        col = index % grid.info.width
-        row = index // grid.info.width
-
-        # Scale cell dimensions by grid resolution to compute position in
-        # dimensions of the map.
-        pos_relative = Vector2(
-            x=col * grid.info.resolution,
-            y=row * grid.info.resolution,
-        )
-
-        # Uniformly pick a random yaw in range [0.0, 2.0 * pi].
-        yaw_relative = rng.uniform(low=0.0, high=2.0 * math.pi)
-
-        # Offset relative position and yaw by origin to obtain absolute values
-        # in map space.
-        pos_absolute = pos_relative + origin_pos
-        yaw_absolute = yaw_relative + origin_yaw
-
-        # Create particle with absolute pose and uniform weights.
-        return Particle(
-            pose=TurtlePose(pos_absolute, yaw_absolute),
-            weight=1.0 / _num_particles,
-        )
-
-    # Create a particle for each free cell in those uniformly sampled.
-    particles = [from_cell(c) for c in cells_selected]
-
-    return particles
--- scripts/particle_filter.py
+++ scripts/particle_filter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Framework for running the particle filter.
+"""
+
 # pyright: reportMissingTypeStubs=false
 
 from dataclasses import dataclass, replace
@@ -9,79 +13,117 @@ from typing import Any, List, Union, Tuple
 
 from nav_msgs.msg import OccupancyGrid
 import rospy
-from rospy_util.vector2 import Vector2
 import rospy_util.vector2 as v2
 from sensor_msgs.msg import LaserScan
 
-from lib import LikelihoodField, Particle, TurtlePose
+from lib import LikelihoodField, Particle, TurtlePose, Vector2
 from lib.controller import Cmd, Controller, Sub, cmd, sub
 import lib.likelihood_field as lf
 from lib.util import compose_many
+
 import particle_cloud as pc
 
 
 ### Model ###
 
+# States to represent the operation of the particle filter.
+
 
 @dataclass
 class AwaitMap:
-    pass
+    """
+    Wait for the occupancy grid from the map server.
+    """
 
 
 @dataclass
 class AwaitPose:
+    """
+    Wait for the first odometry measurement from the robot.
+    """
+
     likelihood_field: LikelihoodField
     particle_cloud: List[Particle]
 
 
 @dataclass
 class Initialized:
+    """
+    Begin updating the particle cloud.
+    """
+
     likelihood_field: LikelihoodField
     particle_cloud: List[Particle]
     pose_most_recent: TurtlePose
     pose_last_update: TurtlePose
 
 
+# Union type for possible robot states.
 Model = Union[AwaitMap, AwaitPose, Initialized]
 
+# Start by awaiting the map from the map server.
 init_model: Model = AwaitMap()
 
-### Events ###
+### Messsages ###
+
+# Messages wrapping items received by ROS subscribers.
 
 
 @dataclass
 class LoadMap:
+    """
+    Received the occupancy grid from the map server.
+    """
+
     map: OccupancyGrid
 
 
 @dataclass
 class Move:
+    """
+    Received an odometry measurement from the robot.
+    """
+
     pose: TurtlePose
 
 
 @dataclass
 class Scan:
+    """
+    Received a distance scan from the robot.
+    """
+
     scan: LaserScan
 
 
+# Union type for possible messages.
 Msg = Union[LoadMap, Move, Scan]
 
 ### Update ###
 
+# The number of particles to simulate in the cloud.
 NUM_PARTICLES: int = 10000
 
+# Linear and angular movement thresholds necessary for the particle filter to update.
 MVMT_THRESH_LIN: float = 0.2
 MVMT_THRESH_ANG: float = math.pi / 6.0
 
+# Factors for adding noise to the movement of the particles.
 NOISE_LIN: float = 0.1
 NOISE_ANG: float = 0.4
 
+# Standard deviation for computing simulated scan probabilities.
 OBSTACLE_DIST_SD: float = 0.15
 
+# Number of laser scan ranges to consider when updating particle weights.
 NUM_RANGES: int = 6
 
 
 def pose_displacement(p1: TurtlePose, p2: TurtlePose) -> Tuple[Vector2, float]:
+    """
+    Calculate the robot's linear and angular displacement relative to its pose during
+    the last particle cloud update.
+    """
     displacement_linear = v2.rotate(p1.position - p2.position, -1.0 * p1.yaw)
     displacement_angular = p1.yaw - p2.yaw
     return (displacement_linear, displacement_angular)
@@ -94,6 +136,10 @@ def update_particle_cloud(
     disp_angular: float,
     scan: LaserScan,
 ) -> List[Particle]:
+    """
+    Update the poses and weights for the particle cloud and resample particles
+    with replacement.
+    """
     return compose_many(
         pc.resample,
         pc.normalize,
@@ -112,6 +158,12 @@ def update_particle_cloud(
 
 
 def update(msg: Msg, model: Model) -> Tuple[Model, List[Cmd[Any]]]:
+    """
+    Update the particle filter depending on the message received and the current
+    state.
+    """
+
+    # Switch the state from AwaitMap to AwaitPose upon receiving the occupancy grid.
     if isinstance(model, AwaitMap) and isinstance(msg, LoadMap):
         cloud_init = pc.normalize(
             pc.initialize(num_particles=NUM_PARTICLES, map=msg.map)
@@ -127,6 +179,7 @@ def update(msg: Msg, model: Model) -> Tuple[Model, List[Cmd[Any]]]:
             [cmd.init_particle_cloud(cloud_init, frame_id="map")],
         )
 
+    # Switch the state from AwaitPose to Initialized upon receiving the robot odometry.
     if isinstance(model, AwaitPose) and isinstance(msg, Move):
         return (
             Initialized(
@@ -138,9 +191,13 @@ def update(msg: Msg, model: Model) -> Tuple[Model, List[Cmd[Any]]]:
             cmd.none,
         )
 
+    # Update the robot's pose upon receiving a movement command.
     if isinstance(model, Initialized) and isinstance(msg, Move):
         return (replace(model, pose_most_recent=msg.pose), cmd.none)
 
+    # Update the particle cloud upon receiving a distance scan if the robot has
+    # moved beyond the specified thresholds since the last update.
+    # Then, publish the estimated robot pose and the updated particle cloud.
     if isinstance(model, Initialized) and isinstance(msg, Scan):
         (disp_lin, disp_ang) = pose_displacement(
             p1=model.pose_most_recent,
@@ -185,12 +242,19 @@ def update(msg: Msg, model: Model) -> Tuple[Model, List[Cmd[Any]]]:
 
 
 def subscriptions(model: Model) -> List[Sub[Any, Msg]]:
+    """
+    Subscribe to ROS topics that are necessary for the current particle filter
+    state.
+    """
     if isinstance(model, AwaitMap):
+        # While awaiting the map, subscribe to the map server.
         return [sub.occupancy_grid(LoadMap)]
 
     if isinstance(model, AwaitPose):
+        # While awaiting the first robot pose, subscribe to movement messages.
         return [sub.odometry(Move)]
 
+    # Once initialized, subscribe to movement and scan messages.
     return [sub.odometry(Move), sub.laser_scan(Scan)]
 
 
@@ -198,6 +262,10 @@ def subscriptions(model: Model) -> List[Sub[Any, Msg]]:
 
 
 def run() -> None:
+    """
+    Instantiate a ROS node, set up the callback function for relevant ROS topics,
+    and listen for messages.
+    """
     rospy.init_node("turtlebot3_particle_filter")
 
     Controller.run(
